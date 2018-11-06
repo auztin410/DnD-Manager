23025b3de37e8cc20ff4d2f8841278e0bac0a6a0
auztin410 <auztin410@gmail.com>
2018-11-06 16:01:11 -0500

    Changed styles up for sections a bit.

--- src/components/WorldShakingEvent.jsx
+++ src/components/WorldShakingEvent.jsx
@@ -5,15 +5,15 @@ const WorldShakingEvent = props => {
     if(!props.worldShakingDetails) {
         return (
             <div className="bigEvent">
-                <div className="container"><p className="property">{props.worldShakingEvent}</p></div>
+                <div className="container"><p className="property">Event: {props.worldShakingEvent}</p></div>
             </div>
         )
     }
     else if(props.worldShakingDetails) {
         return (
             <div className="bigEvent">
-                <div className="container"><p className="property">{props.worldShakingEvent}</p></div>
-                <div className="container"><p className="property">{props.worldShakingDetails}</p></div>
+                <div className="container"><p className="property">Event: {props.worldShakingEvent}</p></div>
+                <div className="container"><p className="property">Details: {props.worldShakingDetails}</p></div>
             </div>
         )
     }
