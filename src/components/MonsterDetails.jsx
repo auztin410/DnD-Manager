0e325be24d1ece9abbcd696ad5a87bf138824d85
auztin410 <auztin410@gmail.com>
2018-11-22 10:28:53 -0500

    built out more of the monster details, but need to work on ternary operator for some details.

--- src/components/MonsterDetails.jsx
+++ src/components/MonsterDetails.jsx
@@ -3,10 +3,25 @@ import '../App.css';
 
 const MonsterDetails = props => (
 
-    <div>
+    <div className="monsterDetails">
+        <div className="plaque">
         <h2>{props.monster.name}</h2>
-        <p>Size: {props.monster.size} | Type: {props.monster.type} | Subtype: {props.monster.subtype} | Alignment: {props.monster.alignment}</p>
-        <p>Challenge Rating: {props.monster.challenge_rating} | Armor Class: {props.monster.armor_class}</p>
+        </div>
+        <div className="monsterGrouping">
+            <p>Size: {props.monster.size} | Type: {props.monster.type} | Subtype: {props.monster.subtype}</p>
+            <p>Challenge Rating: {props.monster.challenge_rating} | Alignment: {props.monster.alignment}</p>
+        </div>
+        <div className="monsterGrouping">
+            <h4>Base Stats</h4>
+            <p>Strength: {props.monster.strength} | Dexterity: {props.monster.dexterity} | Constitution: {props.monster.constitution}</p>
+            <p>Intelligence: {props.monster.intelligence} | Wisdom: {props.monster.wisdom} | Charisma: {props.monster.charisma}</p>
+        </div>
+        <div className="monsterGrouping">
+            <h4>Additional Stats</h4>
+            <p>Hit points: {props.monster.hit_points} | Hit dice: {props.monster.hit_dice}</p>
+            <p>Speed: {props.monster.speed}</p>
+        </div>
+
     </div>
 )
 
