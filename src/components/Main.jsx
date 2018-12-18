2189ac35249c0f72adcd9217fd9fb8fe717ca6a4
auztin410 <auztin410@gmail.com>
2018-12-18 18:36:37 -0500

    background selectors working!

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,6 +26,7 @@ import ReactHowler from 'react-howler'
 import { blackLotus, nightSounds, darkWinds, rumination, beach, bigStorm, carnivalRide, dryLava, earthquake, fire, firePlace, forestFire, forest, heartbeat, hurricane, jungle, noMoreMagic, ocean, rain, rainThunder, song18, storm, swamp, loomingBattle, wind, windHowl } from '../assets/Music';
 import Spells from '../assets/Json/Spells';
 import RaceList from '../assets/Json/RaceList';
+import BackgroundList from '../assets/Json/BackgroundList';
 
 
 
@@ -148,6 +149,12 @@ class Main extends Component {
             characterSubRace: "",
             characterClass: "",
             characterBackground: "",
+            characterBackgroundSelected: null,
+            characterPersonalityTrait1: "",
+            characterPersonalityTrait2: "",
+            characterIdeal: "",
+            characterBond: "",
+            characterFlaw: "",
             characterAlignment: "",
             characterSubRaceOptions: [],
             statRollsRandom: [],
@@ -189,6 +196,7 @@ class Main extends Component {
         this.economicStatus = this.economicStatus.bind(this);
         this.handleGenerateStatRolls = this.handleGenerateStatRolls.bind(this);
         this.handleStandardSetStats = this.handleStandardSetStats.bind(this);
+        this.handleChangeCharacterCreation = this.handleChangeCharacterCreation.bind(this);
 
         // Delete these after testing is complete.
         this.handleTestWeapons = this.handleTestWeapons.bind(this);
@@ -224,48 +232,6 @@ class Main extends Component {
         this.setState({
             [event.target.name]: event.target.value
         });
-        let name = event.target.name;
-        let value = event.target.value;
-        let options;
-        if (name === "characterRace") {
-            switch (value) {
-                default:
-                    this.setState({
-                        characterSubRaceOptions: [],
-                    });
-                    break;
-                case ("dragonborn"):
-                    options = RaceList.filter(obj => obj.Race === value);
-                    this.setState({
-                        characterSubRaceOptions: options,
-                    });
-                    break;
-                case ("dwarf"):
-                    options = RaceList.filter(obj => obj.Race === value);
-                    this.setState({
-                        characterSubRaceOptions: options,
-                    });
-                    break;
-                case ("elf"):
-                    options = RaceList.filter(obj => obj.Race === value);
-                    this.setState({
-                        characterSubRaceOptions: options,
-                    });
-                    break;
-                case ("gnome"):
-                    options = RaceList.filter(obj => obj.Race === value);
-                    this.setState({
-                        characterSubRaceOptions: options,
-                    });
-                    break;
-                case ("halfling"):
-                    options = RaceList.filter(obj => obj.Race === value);
-                    this.setState({
-                        characterSubRaceOptions: options,
-                    });
-                    break;
-            }
-        }
     };
 
     handleCheckBox(event) {
@@ -4414,6 +4380,118 @@ class Main extends Component {
 
     };
 
+    handleChangeCharacterCreation(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        let name = event.target.name;
+        let value = event.target.value;
+        let options;
+        if (name === "characterRace") {
+            switch (value) {
+                default:
+                    this.setState({
+                        characterSubRaceOptions: [],
+                    });
+                    break;
+                case ("dragonborn"):
+                    options = RaceList.filter(obj => obj.Race === value);
+                    this.setState({
+                        characterSubRaceOptions: options,
+                    });
+                    break;
+                case ("dwarf"):
+                    options = RaceList.filter(obj => obj.Race === value);
+                    this.setState({
+                        characterSubRaceOptions: options,
+                    });
+                    break;
+                case ("elf"):
+                    options = RaceList.filter(obj => obj.Race === value);
+                    this.setState({
+                        characterSubRaceOptions: options,
+                    });
+                    break;
+                case ("gnome"):
+                    options = RaceList.filter(obj => obj.Race === value);
+                    this.setState({
+                        characterSubRaceOptions: options,
+                    });
+                    break;
+                case ("halfling"):
+                    options = RaceList.filter(obj => obj.Race === value);
+                    this.setState({
+                        characterSubRaceOptions: options,
+                    });
+                    break;
+            }
+        }
+        else if (name === "characterBackground") {
+            let selected;
+            console.log(value);
+            switch (value) {
+                default: 
+                selected = BackgroundList.find(item => item.BackgroundValue === value);
+                this.setState({
+                    characterBackgroundSelected: selected,
+                });
+                break;
+                case (""):
+                this.setState({
+                    characterBackgroundSelected: null,
+                    characterPersonalityTrait1: "",
+                    characterPersonalityTrait2: "",
+                    characterIdeal: "",
+                    characterBond: "",
+                    characterFlaw: "",
+                });
+                break;
+                // case ("charlatan"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+                // case ("criminal"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+                // case ("entertainer"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+                // case ("folk hero"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+                // case ("guild artisan"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+                // case ("hermit"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+                // case ("noble"):
+                // selected = BackgroundList.find(item => item.BackgroundValue === value);
+                // this.setState({
+                //     characterBackgroundSelected: selected,
+                // });
+                // break;
+            }
+        }
+    };
+
     render() {
 
         if (this.state.npcComponent === true) {
@@ -5600,9 +5678,9 @@ class Main extends Component {
                         <div className="visible">
                             <button onClick={this.handleSpellClick}>Random Spell Console</button>
 
-                            <h2>Character's Name</h2><input onChange={this.handleChange} type="text" name="characterName" />
+                            <h2>Character's Name</h2><input onChange={this.handleChangeCharacterCreation} type="text" name="characterName" />
                             <h4>Race</h4>
-                            <select onChange={this.handleChange} name="characterRace">
+                            <select onChange={this.handleChangeCharacterCreation} name="characterRace">
                                 <option value="">None Selected</option>
                                 <option value="dragonborn">Dragonborn</option>
                                 <option value="dwarf">Dwarf</option>
@@ -5618,7 +5696,7 @@ class Main extends Component {
                                 ?
                                 <div>
                                     <h4>Sub Race</h4>
-                                    <select onChange={this.handleChange} name="characterSubRace">
+                                    <select onChange={this.handleChangeCharacterCreation} name="characterSubRace">
                                         {this.state.characterSubRaceOptions.map(item => (
                                             <option key={item.SubRaceValue} value={item.SubRaceValue}>{item.SubRaceName}</option>
                                         ))}
@@ -5627,7 +5705,7 @@ class Main extends Component {
                                 : null
                             }
                             <h4>Class</h4>
-                            <select onChange={this.handleChange} name="characterClass">
+                            <select onChange={this.handleChangeCharacterCreation} name="characterClass">
                                 <option value="">None Selected</option>
                                 <option value="barbarian">Barbarian</option>
                                 <option value="bard">Bard</option>
@@ -5663,13 +5741,13 @@ class Main extends Component {
                             <br />
                             <button onClick={this.handleGenerateStatRolls}>Random</button> {" "} <button onClick={this.handleStandardSetStats}>Standard Set</button>
                             <br />
-                            <select onChange={this.handleChange} name="characterBackground">
+                            <select onChange={this.handleChangeCharacterCreation} name="characterBackground">
                                 <option value="">None Selected</option>
                                 <option value="acolyte">Acolyte</option>
                                 <option value="charlatan">Charlatan</option>
                                 <option value="criminal">Criminal</option>
                                 <option value="entertainer">Entertainer</option>
-                                <option value="folk here">Folk Hero</option>
+                                <option value="folk hero">Folk Hero</option>
                                 <option value="guild artisan">Guild Artisan</option>
                                 <option value="hermit">Hermit</option>
                                 <option value="noble">Noble</option>
@@ -5679,8 +5757,83 @@ class Main extends Component {
                                 <option value="soldier">Soldier</option>
                                 <option value="urchin">Urchin</option>
                             </select>
+                            {(this.state.characterBackground === "")
+                            ? null
+                            : 
+                            <div>
+                                <h4>Description</h4>
+                                {this.state.characterBackgroundSelected.Description.map(item => (
+                                    <p key={item}>{item}</p>
+                                ))}
+                                <h4>Skills</h4>
+                                {this.state.characterBackgroundSelected.Skills.map(item => (
+                                    <p key={item}>{item}</p>
+                                ))}
+                                {(this.state.characterBackgroundSelected.Tools.length > 0)
+                                ?
+                                <div>
+                                    <h4>Tools</h4>
+                                    {this.state.characterBackgroundSelected.Tools.map(item => (
+                                        <p key={item}>{item}</p>
+                                    ))}
+                                </div>
+                                : null
+                                }
+                                {(this.state.characterBackgroundSelected.Languages.length > 0)
+                                ?
+                                <div>
+                                    <h4>Languages</h4>
+                                    <p>{this.state.characterBackgroundSelected.Languages[0]}</p>
+                                </div>
+                                : null
+                                }
+                                <h4>Equipment</h4>
+                                {this.state.characterBackgroundSelected.Equipment.map(item => (
+                                    <p key={item}>{item}</p>
+                                ))}
+                                <h4>Starting Currency: {this.state.characterBackgroundSelected.Currency} GP</h4>
+                                <h4>Feature: {this.state.characterBackgroundSelected.FeatureName}</h4>
+                                {this.state.characterBackgroundSelected.Feature.map(item => (
+                                    <p key={item}>{item}</p>
+                                ))}
+                                <h4>Personality Traits</h4>
+                                <select onChange={this.handleChangeCharacterCreation} name="characterPersonalityTrait1">
+                                <option value="">None Selected</option>
+                                {this.state.characterBackgroundSelected.PersonalityTraits.map(item => (
+                                    <option key={item} value={item}>{item}</option>
+                                ))}
+                                </select>
+                                <select onChange={this.handleChangeCharacterCreation} name="characterPersonalityTrait2">
+                                <option value="">None Selected</option>
+                                {this.state.characterBackgroundSelected.PersonalityTraits.map(item => (
+                                    <option key={item} value={item}>{item}</option>
+                                ))}
+                                </select> 
+                                <h4>Ideals</h4>
+                                <select onChange={this.handleChangeCharacterCreation} name="characterIdeal">
+                                <option value="">None Selected</option>
+                                {this.state.characterBackgroundSelected.Ideals.map(item => (
+                                    <option key={item} value={item}>{item}</option>
+                                ))}
+                                </select>
+                                <h4>Bond</h4>
+                                <select onChange={this.handleChangeCharacterCreation} name="characterBond">
+                                <option value="">None Selected</option>
+                                {this.state.characterBackgroundSelected.Bonds.map(item => (
+                                    <option key={item} value={item}>{item}</option>
+                                ))}
+                                </select>
+                                <h4>Flaw</h4>
+                                <select onChange={this.handleChangeCharacterCreation} name="characterFlaw">
+                                <option value="">None Selected</option>
+                                {this.state.characterBackgroundSelected.Flaws.map(item => (
+                                    <option key={item} value={item}>{item}</option>
+                                ))}
+                                </select>
+                            </div>
+                            }
                             <br />
-                            <select onChange={this.handleChange} name="characterAlignment">
+                            <select onChange={this.handleChangeCharacterCreation} name="characterAlignment">
                                 <option value="">None Selected</option>
                                 <option value="lawful good">Lawful Good</option>
                                 <option value="neutral good">Neutral Good</option>
