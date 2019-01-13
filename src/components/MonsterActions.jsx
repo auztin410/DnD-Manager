da50f7f90653a8ee505cdb4ec79476e85ea7135d
auztin410 <auztin410@gmail.com>
2019-01-13 08:38:25 -0500

    Monster abilities cleaned up into tables.

--- src/components/MonsterActions.jsx
+++ src/components/MonsterActions.jsx
@@ -3,20 +3,42 @@ import '../App.css';
 
 
 const MonsterActions = props => (
-<div className="scrollDiv">
+    <div className="scrollDiv">
         <div className="plaque">
             <h4>Actions</h4>
         </div>
-            {props.monster.actions.map(({name, desc, attack_bonus, damage_dice, damage_bonus}) => (
-                <div key={name} className="monsterGrouping">
+        {/* {props.monster.actions.map(({ name, desc, attack_bonus, damage_dice, damage_bonus }) => (
+            <div key={name} className="monsterGrouping">
                 <p>Action: {name}</p>
                 <p>Description: {desc}</p>
                 <p>Attack Bonus: {attack_bonus}</p>
                 {damage_dice && <p>Damage Dice: {damage_dice}</p>}
                 {damage_bonus && <p>Damage Bonus: {damage_bonus}</p>}
-                </div>
-            ))}
-        </div>
+            </div>
+        ))} */}
+
+        {props.monster.actions.map(({ name, desc, attack_bonus, damage_dice, damage_bonus }) => (
+            <div key={name} className="monsterGrouping">
+            <table>
+                <tr>
+                    <th className="profRow">Action</th>
+                    <th className="profRow">Description</th>
+                    <th className="profRow">Attack Bonus</th>
+                    {damage_dice && <th className="profRow">Damage Dice</th>}
+                    {damage_bonus && <th className="profRow">Damage Bonus</th>}
+                </tr>
+                <tr>
+                    <td className="profRow">{name}</td>
+                    <td className="profRow">{desc}</td>
+                    <td className="profRow">{attack_bonus}</td>
+                    {damage_dice && <td className="profRow">{damage_dice}</td>}
+                    {damage_bonus && <td className="profRow">{damage_bonus}</td>}
+                </tr>
+            </table>
+            </div>
+        ))}
+    </div>
+
 );
 
 export default MonsterActions;
--- src/components/MonsterSpecialAbilities.jsx
+++ src/components/MonsterSpecialAbilities.jsx
@@ -7,7 +7,7 @@ const MonsterSpecialAbilities = props => (
     <div className="plaque">
             <h4>Special Abilities</h4>
         </div>
-        {props.monster.special_abilities.map(({name, desc, attack_bonus, damage_dice, damage_bonus}) => (
+        {/* {props.monster.special_abilities.map(({name, desc, attack_bonus, damage_dice, damage_bonus}) => (
                 <div className="monsterGrouping" key={name}>
                 <p>Action: {name}</p>
                 <p>Description: {desc}</p>
@@ -15,7 +15,27 @@ const MonsterSpecialAbilities = props => (
                 {damage_dice && <p>Damage Dice: {damage_dice}</p>}
                 {damage_bonus && <p>Damage Bonus: {damage_bonus}</p>}
                 </div>
-            ))}
+            ))} */}
+            {props.monster.special_abilities.map(({ name, desc, attack_bonus, damage_dice, damage_bonus }) => (
+            <div key={name} className="monsterGrouping">
+            <table>
+                <tr>
+                    <th className="profRow">Action</th>
+                    <th className="profRow">Description</th>
+                    <th className="profRow">Attack Bonus</th>
+                    {damage_dice && <th className="profRow">Damage Dice</th>}
+                    {damage_bonus && <th className="profRow">Damage Bonus</th>}
+                </tr>
+                <tr>
+                    <td className="profRow">{name}</td>
+                    <td className="profRow">{desc}</td>
+                    <td className="profRow">{attack_bonus}</td>
+                    {damage_dice && <td className="profRow">{damage_dice}</td>}
+                    {damage_bonus && <td className="profRow">{damage_bonus}</td>}
+                </tr>
+            </table>
+            </div>
+        ))}
     </div>
 );
 
