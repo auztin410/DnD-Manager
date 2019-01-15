975bb76fb3a2e3e3aca05cdae676150b85dc242b
auztin410 <auztin410@gmail.com>
2019-01-14 20:10:04 -0500

    Building out form to create new quests.

--- src/components/QuestTracker.jsx
+++ src/components/QuestTracker.jsx
@@ -11,11 +11,21 @@ class QuestTracker extends Component {
             groupedQuests: [],
             showQuest: false,
             selectedQuest: [],
+            questName: "",
+            chainQuest: "",
+            questChainName: "",
+            startNPC: "",
+            startLocation: "",
+            endNPC: "",
+            endLocation: "",
+            experience: "",
+            questDescription: "",
         }
 
         this.handleFindGroup = this.handleFindGroup.bind(this);
         this.handleSelect = this.handleSelect.bind(this);
         this.handleCloseQuest = this.handleCloseQuest.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -27,6 +37,12 @@ class QuestTracker extends Component {
             groupedQuests,
             groupedTitles,
         })
+    };
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     };
 
     handleSelect(event) {
@@ -49,7 +65,7 @@ class QuestTracker extends Component {
     handleFindGroup(title) {
         console.log(title);
         let found = this.state.groupedQuests.filter(item => item.Group === title);
-        let sorted = found.sort(function(a,b){
+        let sorted = found.sort(function (a, b) {
             return a.QuestPart - b.QuestPart;
         });
         console.log("Found");
@@ -67,62 +83,94 @@ class QuestTracker extends Component {
     render() {
         return (
             <div className="visible" id="questTracker">
+                <div className="createQuest">
+                    <form>
+                        <span>Quest Name</span><input type="text" name="questName" onChange={this.handleChange}/>
+                        <br/>
+                        <span>Chain Quest</span><select name="chainQuest" onChange={this.handleChange}>
+                            <option value="">Single Quest</option>
+                            <br/>
+                            <option value="new">New Chain</option>
+                            {this.state.groupedTitles.map(item => (
+                                <option key={item} value={item}>{item}</option>
+                            ))}
+                        </select>
+                        {(this.state.chainQuest === "new")
+                            ?
+                            <input type="text" name="questChainName" onChange={this.handleChange}/>
+                            : null
+                        }
+                        <br/>
+                        <span>Start NPC</span><input type="text" name="startNPC" onChange={this.handleChange}/>
+                        <br/>
+                        <span>Start Location</span><input type="text" name="startLocation" onChange={this.handleChange}/>
+                        <br/>
+                        <br/>
+                        <span>End NPC</span><input type="text" name="endNPC" onChange={this.handleChange}/>
+                        <br/>
+                        <span>End Location</span><input type="text" name="endLocation" onChange={this.handleChange}/>
+                        <br/>
+                        <span>Experience</span><input type="number" name="experience" onChange={this.handleChange}/>
+                        <br/>
+                        <span>Description</span><textarea name="questDescription" cols="30" rows="10" onChange={this.handleChange}></textarea>
+                    </form>
+                </div>
                 {(this.state.singleQuests.length > 0)
                     ?
                     <div className="questSection">
-                    <table>
-                        <tr>
-                            <th className="profRow" id="questHeader" colSpan="2">Single Quest</th>
-                        </tr>
-                        {this.state.singleQuests.map(item => (
-                            <tr key={item.Title}>
-                                <td className="profRow" id="questSelect">{item.Title}</td>
-                                <td className="profRow">{(item.Completed === true) ? <div className="circle" id="green"></div> : <div className="circle" id="red"></div>}</td>
+                        <table>
+                            <tr>
+                                <th className="profRow" id="questHeader" colSpan="2">Single Quest</th>
                             </tr>
-                        ))}
-                    </table>
+                            {this.state.singleQuests.map(item => (
+                                <tr key={item.Title}>
+                                    <td className="profRow" id="questSelect">{item.Title}</td>
+                                    <td className="profRow">{(item.Completed === true) ? <div className="circle" id="green"></div> : <div className="circle" id="red"></div>}</td>
+                                </tr>
+                            ))}
+                        </table>
                     </div>
                     : null
                 }
                 {this.state.groupedTitles.map(item => (
-                                <div key={item} className="questSection">
-                                <table>
-                                    <tr>
-                                        <th className="profRow" id="questHeader" colSpan="2">{item}</th>
-                                    </tr>
-                                    {this.handleFindGroup(item)}
-                                </table>
-                                </div>
-                        ))}
-                {(this.state.showQuest === true)
-                ?
-                <div className="questSelected">
-                    <span className="closeDisplayItem" onClick={this.handleCloseQuest}>X</span>
-                    <h4>{this.state.selectedQuest.Title}</h4>
-                    <p>Start: {this.state.selectedQuest.Start.NPC} | {this.state.selectedQuest.Start.Location}</p>
-                    <p>End: {this.state.selectedQuest.End.NPC} | {this.state.selectedQuest.End.Location}</p>
-                    <div className="questDescription">
-                        {this.state.selectedQuest.Description}
-                    </div>
-                    <p>Experience: {this.state.selectedQuest.EXP}</p>
-                    {(this.state.selectedQuest.Reward.length > 0)
-                    ?
-                    <table>
-                        <tr>
-                            <th className="profRow" colSpan="2">Rewards</th>
-                        </tr>
-                        
-                        {this.state.selectedQuest.Reward.map(item => (
-                            <tr key={item.Name}>
-                                <td className="profRow">{item.Name}</td>
-                                <td className="profRow">{item.Quantity}</td>
+                    <div key={item} className="questSection">
+                        <table>
+                            <tr>
+                                <th className="profRow" id="questHeader" colSpan="2">{item}</th>
                             </tr>
-                        ))}
-                    </table>
+                            {this.handleFindGroup(item)}
+                        </table>
+                    </div>
+                ))}
+                {(this.state.showQuest === true)
+                    ?
+                    <div className="questSelected">
+                        <span className="closeDisplayItem" onClick={this.handleCloseQuest}>X</span>
+                        <h4>{this.state.selectedQuest.Title}</h4>
+                        <p>Start: {this.state.selectedQuest.Start.NPC} | {this.state.selectedQuest.Start.Location}</p>
+                        <p>End: {this.state.selectedQuest.End.NPC} | {this.state.selectedQuest.End.Location}</p>
+                        <div className="questDescription">
+                            {this.state.selectedQuest.Description}
+                        </div>
+                        <p>Experience: {this.state.selectedQuest.EXP}</p>
+                        {(this.state.selectedQuest.Reward.length > 0)
+                            ?
+                            <table>
+                                <tr>
+                                    <th className="profRow" colSpan="2">Rewards</th>
+                                </tr>
+
+                                {this.state.selectedQuest.Reward.map(item => (
+                                    <tr key={item.Name}>
+                                        <td className="profRow">{item.Name}</td>
+                                        <td className="profRow">{item.Quantity}</td>
+                                    </tr>
+                                ))}
+                            </table>
+                            : null
+                        }
+                    </div>
                     : null
-                    }
-                </div>
-                : null
                 }
             </div>
         )
